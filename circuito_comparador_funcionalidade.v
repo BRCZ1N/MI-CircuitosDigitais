d5c3a71f6bb3f06330aeb0c7f6bd6243d563a5eb
BRCZ1N <brunocamposo.r@gmail.com>
2023-08-28 16:39:12 -0300

    Alterações

--- circuito_comparador_funcionalidade.v
+++ circuito_comparador_funcionalidade.v
@@ -1,6 +1,7 @@
-module circuito_comparador_funcionalidade(A,B,C,D,E,F);	
+module circuito_comparador_funcionalidade(A,B,C,D,E,F,FI);	
 
 	input A,B,C,D,E,F;
+	output FI;
 	wire NA = !A, NB = !B, NC = !C, ND = !D, NE = !E, NF = !F;
 	
 	wire NA_and_NB_and_NC,ND_and_NF_and_NE, NA_and_NB_and_NC_and_ND_and_NF_and_NE;
@@ -9,7 +10,7 @@ module circuito_comparador_funcionalidade(A,B,C,D,E,F);
 	
 	wire NA_and_B_and_C,ND_and_F_and_E, NA_and_B_and_C_and_ND_and_F_and_E;
 	wire A_and_NB_and_NC,D_and_NF_and_NE, A_and_NB_and_NC_and_D_and_NF_and_NE; 
-	wire NA_and_NB_and_C,D_and_NF_and_E, NA_and_NB_and_C_and_D_and_NF_and_E;
+	wire D_and_NF_and_E, NA_and_NB_and_C_and_D_and_NF_and_E;
 	
 	wire A_and_B_and_NC,D_and_F_and_NE, A_and_B_and_NC_and_D_and_F_and_NE; 
 	wire A_and_B_and_C,D_and_F_and_E, A_and_B_and_C_and_D_and_F_and_E;
@@ -22,18 +23,34 @@ module circuito_comparador_funcionalidade(A,B,C,D,E,F);
 	and(NA_and_NB_and_C,NA,NC,C);
 	and(A_and_B_and_NC,A,B,NC);
 	and(A_and_B_and_C,A,B,C);
+	and(ND_and_NF_and_NE,ND,NF,NE);
+	and(ND_and_NF_and_E,ND,NF,E);
+	and(ND_and_F_and_NE,ND,F,NE);
+	and(ND_and_F_and_E,ND,F,E);
+	and(D_and_NF_and_NE,D,NF,NE);
+	and(D_and_NF_and_E,D,NF,E);
+	and(D_and_F_and_NE,D,F,NE);
+	and(D_and_F_and_E,D,F,E);
 	
-	and(ND_and_NF_and_NE,)
-	and(ND_and_NF_and_E,)
-	and(ND_and_F_and_NE,)
-	and(ND_and_F_and_E,)
-	and(D_and_NF_and_NE,)
-	and(D_and_NF_and_E,)
-	and(D_and_F_and_NE,)
-	and(D_and_F_and_E,)
+	and(NA_and_NB_and_NC_and_ND_and_NF_and_NE,NA_and_NB_and_NC,ND_and_NF_and_NE);
+	and(NA_and_NB_and_C_and_ND_and_NF_and_E,NA_and_NB_and_C,ND_and_NF_and_E);
+	and(NA_and_B_and_NC_and_ND_and_F_and_NE,NA_and_B_and_NC,ND_and_F_and_NE);
+	and(NA_and_B_and_C_and_ND_and_F_and_E,NA_and_B_and_C,ND_and_F_and_E);
+	and(NA_and_B_and_C_and_ND_and_F_and_E,A_and_NB_and_NC,D_and_NF_and_NE);
+	and(NA_and_NB_and_C_and_D_and_NF_and_E,NA_and_NB_and_C,D_and_NF_and_E);
+	and(A_and_B_and_NC_and_D_and_F_and_NE,A_and_B_and_NC,D_and_F_and_NE);
+	and(A_and_B_and_C_and_D_and_F_and_E,A_and_B_and_C,D_and_F_and_E);
+	
+	or(NA_and_NB_and_NC_and_ND_and_NF_and_NE_or_NA_and_NB_and_C_and_ND_and_NF_and_E,NA_and_NB_and_NC_and_ND_and_NF_and_NE,NA_and_NB_and_C_and_ND_and_NF_and_E);
+	or(NA_and_B_and_NC_and_ND_and_F_and_NE_or_NA_and_B_and_C_and_ND_and_F_and_E,NA_and_B_and_NC_and_ND_and_F_and_NE,NA_and_B_and_C_and_ND_and_F_and_E);
+	or(NA_and_B_and_C_and_ND_and_F_and_E_or_NA_and_B_and_C_and_ND_and_F_and_E,NA_and_B_and_C_and_ND_and_F_and_E,NA_and_B_and_C_and_ND_and_F_and_E);
+	or(A_and_B_and_NC_and_D_and_F_and_NE_or_A_and_B_and_C_and_D_and_F_and_E,A_and_B_and_NC_and_D_and_F_and_NE,A_and_B_and_C_and_D_and_F_and_E);
 	
 	
+	or(NA_and_NB_and_NC_and_ND_and_NF_and_NE_or_NA_and_NB_and_C_and_ND_and_NF_and_E__or__NA_and_B_and_NC_and_ND_and_F_and_NE_or_NA_and_B_and_C_and_ND_and_F_and_E,NA_and_NB_and_NC_and_ND_and_NF_and_NE_or_NA_and_NB_and_C_and_ND_and_NF_and_E,NA_and_B_and_NC_and_ND_and_F_and_NE_or_NA_and_B_and_C_and_ND_and_F_and_E);
+	or(NA_and_B_and_C_and_ND_and_F_and_E_or_NA_and_B_and_C_and_ND_and_F_and_E__or__A_and_B_and_NC_and_D_and_F_and_NE_or_A_and_B_and_C_and_D_and_F_and_E,NA_and_B_and_C_and_ND_and_F_and_E_or_NA_and_B_and_C_and_ND_and_F_and_E,A_and_B_and_NC_and_D_and_F_and_NE_or_A_and_B_and_C_and_D_and_F_and_E);
 	
+	or(FI,NA_and_NB_and_NC_and_ND_and_NF_and_NE_or_NA_and_NB_and_C_and_ND_and_NF_and_E__or__NA_and_B_and_NC_and_ND_and_F_and_NE_or_NA_and_B_and_C_and_ND_and_F_and_E,NA_and_B_and_C_and_ND_and_F_and_E_or_NA_and_B_and_C_and_ND_and_F_and_E__or__A_and_B_and_NC_and_D_and_F_and_NE_or_A_and_B_and_C_and_D_and_F_and_E);
 	
 	
 	
@@ -41,8 +58,6 @@ module circuito_comparador_funcionalidade(A,B,C,D,E,F);
 
 	
 	//a'b'c'd'f'e' 
-	
-	
 	
 	
 	//a'b'cd'f'e 
