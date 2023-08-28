4dd522d2561b6724a01d4f092011f17c47f0d5ea
BRCZ1N <brunocamposo.r@gmail.com>
2023-08-28 16:53:20 -0300

    Alteração

--- decodificador_leds.v
+++ decodificador_leds.v
@@ -1,6 +1,7 @@
-module decodificador_leds(A,B,C);
+module decodificador_leds(A,B,C,LED0,LED1,LED2,LED3,LED4,LED5,LED6);
 
 	input A, B, C;
-
-
+	output LED0,LED1,LED2,LED3,LED4,LED5,LED6;
+	wire NA = !A, NB = !B, NC = !C
+	
 endmodule 
