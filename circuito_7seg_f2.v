fa4605079cf24f8040d5c744ab33fdc479d9576a
Alexjr <88115079+Oguelo@users.noreply.github.com>
2023-08-28 20:16:38 -0300

    circuito_7seg_f2

--- circuito_7seg_f2.v
+++ circuito_7seg_f2.v
@@ -1,6 +1,10 @@
-module circuito_7seg_f2(A,B,C);
+module circuito_7seg_f2(A,B,C,D,E,F,F2U1,F2U2);
 
-	input A, B, C;
-
+	input A,B,C,D,E,F;
+	output F2U1,F2U2;
+	wire NA = !A, NB = !B, NC = !C, ND = !D, NE = !E, NF = !F;
+	
+	and(F1U1,NA,B,NC);
+	and(F2U2,ND,E,NF);
 
 endmodule 
