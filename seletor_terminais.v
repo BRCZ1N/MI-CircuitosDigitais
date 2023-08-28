074f1d72248bc989d1cb95180289d12afa730e85
Alexjr <88115079+Oguelo@users.noreply.github.com>
2023-08-28 16:55:37 -0300

    seletor terminais

--- seletor_terminais.v
+++ seletor_terminais.v
@@ -1,6 +1,16 @@
-module seletor_terminais(A,B,C);
+module seletor_terminais(A,B,C,D,E,F, TERMINAL1,TERMINAL2);
 
-	input A, B, C;
-
-
+	input A, B, C, D, E, F;
+	output TERMINAL1,TERMINAL2;
+	wire NA = !A, NB = !B, NC = !C, ND = !D, NE = !E, NF = !F;
+	wire NA_and_NB, ND_and_NF, NC_and_E, NC_and_D, ND_and_NF_or_NC_and_E;
+	
+	and (TERMINAL1,NA,NB);
+	and (ND_and_NF,ND,NF);
+	and (NC_and_E,NC,E);
+	and (NC_and_D,NC,D);
+	
+	
+	or(ND_and_NF_or_NC_and_E,ND_and_NF,NC_and_E);
+	or(TERMINAL2,D_and_NF_or_NC_and_E,NC_and_D);
 endmodule 
