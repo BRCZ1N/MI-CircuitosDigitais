d2195a763719f8476827f1bb5501c198259d0216
BRCZ1N <brunocamposo.r@gmail.com>
2023-08-28 17:17:02 -0300

    Alteração

--- decodificador_matriz.v
+++ decodificador_matriz.v
@@ -1,9 +1,16 @@
-module decodificador_matriz(A,B,C,MLED0,MLED1,LED2,LED3,LED4,LED5,LED6,LED7);
+module decodificador_matriz(A,B,C,MLED0,MLED1,MLED2,MLED3,MLED4,MLED5,MLED6,MLED7);
 
 	input A, B, C;
-	output MLED0,MLED1,LED2,LED3,LED4,LED5,LED6,LED7;
+	output MLED0,MLED1,MLED2,MLED3,MLED4,MLED5,MLED6,MLED7;
 	wire NA = !A, NB = !B, NC = !C;
 	
-	
+	or(MLED0,C,B,A);
+	or(MLED1,NC,B,A);
+	or(MLED2,NB,C,A);
+	or(MLED3,NB,NC,NA);
+	or(MLED4,NA,C,B);
+	or(MLED5,NA,NC,B);
+	or(MLED6,NA,NB,C);
+	or(MLED7,NA,NB,NC);
 
 endmodule 
